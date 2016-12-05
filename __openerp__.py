48cb0893d928d02f92e7275e182678c6b94fcee4
Diatlenko Oleg <you@example.com>
2016-12-05 12:30:21 +0200

    dashboard

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -31,7 +31,9 @@
         'views/templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
-
+        'views/session_workflow.xml',
+        'views/session_board.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
